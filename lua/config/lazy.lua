22c476dbeeda6a11f1a6f3d2188a43054d3ee69d
Doja-Ant <doubiw@foxmail.com>
2024-06-17 13:13:27 +0800

    配置nvim_tree路径改变，和stylua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,23 +17,23 @@ local plugins = {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-live-grep-args.nvim"
+      "nvim-telescope/telescope-live-grep-args.nvim",
     },
     config = function()
-      builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-      vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
-      vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
-      vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, {})
+      vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+      vim.keymap.set("n", "gi", builtin.lsp_implementations, {})
+      vim.keymap.set("n", "gt", builtin.lsp_type_definitions, {})
 
-      vim.keymap.set('n', 'gr', builtin.lsp_references, {})
-      vim.keymap.set('n', '<leader>i', builtin.lsp_incoming_calls, {})
-      vim.keymap.set('n', '<leader>o', builtin.lsp_outgoing_calls, {})
-    end
+      vim.keymap.set("n", "gr", builtin.lsp_references, {})
+      vim.keymap.set("n", "<leader>i", builtin.lsp_incoming_calls, {})
+      vim.keymap.set("n", "<leader>o", builtin.lsp_outgoing_calls, {})
+    end,
   },
   -- nvim tree
   {
@@ -43,64 +43,121 @@ local plugins = {
     },
     config = function()
       require("nvim-tree").setup({
-	on_attach = function(bufnr)
-	  local api = require("nvim-tree.api")
-	  local function opts(desc)
-	    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	  end
-	  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-	  vim.keymap.set('n', '<leader>k', api.tree.change_root_to_parent, opts('Up'))
-	  vim.keymap.set('n', '<leader>j', api.tree.change_root_to_node, opts('CD'))
-	  api.config.mappings.default_on_attach(bufnr)
-	end
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+          local function opts(desc)
+            return {
+              desc = "nvim-tree: " .. desc,
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+              nowait = true,
+            }
+          end
+          api.config.mappings.default_on_attach(bufnr)
+          vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+          vim.keymap.set("n", "<leader>k", api.tree.change_root_to_parent, opts("Up"))
+          vim.keymap.set("n", "<leader>j", api.tree.change_root_to_node, opts("CD"))
+          vim.keymap.set("n", "<leader>e", api.tree.toggle, opts("Toggle"))
+        end,
+        reload_on_bufenter = true,
+        prefer_startup_root = true,
+        -- respect_buf_cwd = true,
+        -- sync_root_with_cwd = true,
+        -- actions = {
+        --   change_dir = {
+        --     global = true,
+        --   },
+        -- },
+        -- update_focused_file = {
+        --  enable = true,
+        --  update_root = true,
+        -- }
+        update_focused_file = {
+          enable = true,
+          update_root = {
+            enable = true,
+          },
+        },
       })
-      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>')
-    end
+      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
+    end,
   },
-  -- lsp config 
+  -- lsp config
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
   -- nvim cmp
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/nvim-cmp',
-  -- color schemes 
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/nvim-cmp",
+  -- formatter
+  {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "isort" },
+          cpp = { "clang-format" },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+        log_level = 1,
+        notify_on_error = true,
+      })
+      require("conform").formatters.stylua = {
+        prepend_args = {
+          "--indent-width",
+          "2",
+          "--indent-type",
+          "Spaces",
+        },
+      }
+    end,
+  },
+  -- color schemes
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-  }
+  },
 }
 
 require("lazy").setup(plugins, opts)
 
 require("mason").setup()
 
-require("mason-lspconfig").setup{
-  ensure_installed = {"clangd"}
-}
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "clangd" },
+})
 
 local cmp = require("cmp")
-cmp.setup {
+cmp.setup({
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-5),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<C-b>"] = cmp.mapping.scroll_docs(-5),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   snippet = {
     expand = function(args)
       vim.snippet.expand(args.body)
-    end
+    end,
   },
   sources = {
-    {name = 'nvim_lsp'}
-  }
-}
+    { name = "nvim_lsp" },
+  },
+})
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require("lspconfig").clangd.setup {
-  capabilities = capabilities
-}
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("lspconfig").clangd.setup({
+  capabilities = capabilities,
+})
+require("lspconfig").lua_ls.setup({
+  capabilities = capabilities,
+})
