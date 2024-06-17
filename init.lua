9afd15ae028c73a129a22d15fa4df53acbacbd82
Doja-Ant <doubiw@foxmail.com>
2024-06-17 13:55:14 +0800

    config relative line number

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
-vim.g.mapleader = ' '
-vim.o.clipboard = 'unnamedplus'
+vim.g.mapleader = " "
+vim.o.clipboard = "unnamedplus"
 
-require('config.keymaps')
-require('config.lazy')
+require("config.keymaps")
+require("config.lazy")
 
-vim.cmd[[colorscheme tokyonight]]
-
-
+vim.cmd([[colorscheme tokyonight]])
