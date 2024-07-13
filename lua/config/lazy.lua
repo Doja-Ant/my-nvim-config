50c59b9e16e930ef8c1e7d58dd6d76b7ae02a74c
Doja-Ant <dja200211@gmail.com>
2024-07-13 14:31:44 +0800

    add py lsp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -121,7 +121,7 @@ require("lazy").setup(plugins, opts)
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "clangd" },
+  ensure_installed = { "lua_ls", "clangd", "pylsp" },
 })
 
 local cmp = require("cmp")
@@ -145,6 +145,10 @@ cmp.setup({
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("lspconfig").clangd.setup({
+  capabilities = capabilities,
+})
+
+require("lspconfig").pylsp.setup({
   capabilities = capabilities,
 })
 
